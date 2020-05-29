f7b2a14ad82eeb363894f2231762f9ff07824172
Calvin <laica@sheridancollege.ca>
2020-05-29 15:35:59 -0400

    Completed Initial Skeleton of the Left Hand Column
    Start work on the main container and right column

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 
 //sliders
 var slider = document.getElementById('song-volume');
+//var updateSliderValue = document.getElementById('slider-update-value');
 
 noUiSlider.create(slider, {
-    start: [ 90 ],
+	start: [ 50 ],
+	behaviour: 'snap',
     connect: [true, false],
 	range: {
 		'min': [   0 ],
 		'max': [ 100 ]
 	}
 });
+
+/*slider.addEventListener('click', function(){
+	slider.noUiSlider.updateSlider({
+		tooltips: true,
+		pips:null
+	});
+});*/
 
 //check if slider is updating
 /*slider.noUiSlider.on('update', function (values, handle) {
